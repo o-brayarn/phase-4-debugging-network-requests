8c32ea44146f925370d95d2df9fc62358363ad89
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-11-26 17:36:58 +0300

    completed debugging network requests

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,5 +4,12 @@ class MoviesController < ApplicationController
     movies = Movie.all
     render json: movies
   end
-
+def create
+  movie = Movie.create(params_movie)
+  render json: movie
+end
+private
+def params_movie
+  params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+end
 end
